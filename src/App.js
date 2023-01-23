451061bccf720b49f24df01e8a4b25335a8754dd
LimLimmm <122277812+LimLimmm@users.noreply.github.com>
2023-01-24 01:06:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/login_component";
 import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
+import {starting_page} from './components/starting_page.js';
+import {homepage} from './components/homepage.js';
+import {matching_page} from './components/matching_page.js';
+import {catalyst_report} from './components/catalyst_report.js';
+import {catalyst_profiling} from './components/catalyst_profiling.js';
 
 function App() {
   return (
@@ -18,6 +23,11 @@ function App() {
               <Route path="/sign-in" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
               <Route path="/userDetails" element={<UserDetails />} />
+              <Route exact path="/" component={homepage} />
+              <Route exact path="/catalyst_profiling" component={catalyst_profiling} />
+              <Route exact path="/catalyst_report" component={catalyst_report} />
+              <Route exact path="/matching_page" component={matching_page} />
+              <Route exact path="/starting_page" component={starting_page} />
             </Routes>
           </div>
         </div>
