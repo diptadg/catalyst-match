2c5c80e5ad35980e9f5fa6836206dea0a8940e0d
diptadg <diptadatta.gupta@sd.taylors.edu.my>
2023-02-21 11:26:42 +0800

    remove old frontend and backend

--- reactui/src/api/endpoints/userEndpoints.js
+++
@@ -1,9 +0,0 @@
-import http from "../../utils/httpRequest";
-
-/**
- * Fetch User Profile API endpoint
- * @param {string} id - ID of user
- * @returns {Promise} - Axios promise object
- */
-export const getUserProfile = () =>
-  http.get(`/me`, { requireAuthHeader: true });
